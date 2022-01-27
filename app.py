d80ea520a35dc2fa799d2f487dd54a7f8407036b
Jędrzej Żytka <j.zytka@wp.pl>
2022-01-27 23:36:57 +0100

    raw version

--- app.py
+++ app.py
@@ -7,18 +7,25 @@ le_dict_pkl = pickle.load(open('le_dict.pkl', 'rb'))
 
 def main():
     st.set_page_config(page_title='Heart Disease Prediction')
+    st.header('Heart Disease Prediction')
 
     with st.form(key='my_form'):
         age = st.slider(label='Age', value=1, min_value=1, max_value=100)
-        sex = st.selectbox(label='Sex', options=('M', 'F'))
-        chest_pain_type = st.selectbox(label='Chest-pain Type', options=('ASY', 'ATA', 'NAP', 'TA'))
-        resting_bp = st.number_input(label='Resting Blood Pressure')
-        cholesterol = st.number_input(label='Cholesterol')
-        fasting_bs = st.selectbox(label='Fasting Blood Sugar', options=[0, 1])
-        resting_ecg = st.selectbox(label='RestingECG ', options=('LVH', 'Normal', 'ST'))
-        max_heart_rate = st.number_input(label='Max Heart Rate')
-        exercise_angina = st.selectbox(label='Exercise Angina', options=('N', 'Y'))
-        st_slope = st.selectbox(label='ST Slope', options=('Down', 'Flat', 'Up'))
+        col1, col2, col3 = st.columns(3)
+        sex = col1.selectbox(label='Sex', options=('M', 'F'))
+        chest_pain_type = col2.selectbox(label='Chest-pain Type', options=('ASY', 'ATA', 'NAP', 'TA'))
+        fasting_bs = col3.selectbox(label='Fasting Blood Sugar', options=(0, 1))
+        
+        col4, col5, col6 = st.columns(3)
+        resting_ecg = col4.selectbox(label='RestingECG ', options=('LVH', 'Normal', 'ST'))
+        exercise_angina = col5.selectbox(label='Exercise Angina', options=('N', 'Y'))
+        st_slope = col6.selectbox(label='ST Slope', options=('Down', 'Flat', 'Up'))
+        
+        col7, col8, col9 = st.columns(3)
+        resting_bp = col7.number_input(label='Resting Blood Pressure', min_value=1, step=1)
+        max_heart_rate = col8.number_input(label='Max Heart Rate', min_value=1, step=1)
+        cholesterol = col9.number_input(label='Cholesterol', min_value=1, step=1)
+        
         submit = st.form_submit_button('Submit')
     
         if submit:
@@ -32,12 +39,14 @@ def main():
                                  'MaxHR': max_heart_rate,
                                  'ExerciseAngina': exercise_angina,
                                  'ST_Slope': st_slope},index=[0])
-        
+            
             for col in le_dict_pkl.keys():
                 data[col] = le_dict_pkl[col].transform(data[col])
-            
+                
             prediction = model_pkl.predict(data)[0]
-            st.write('hello')
-	
-if __name__ == "__main__":
+            st.subheader('Is individual suffering from a heart disease: {0}'.format('Yes' if prediction == 1 else 'No'))
+            
+    st.caption('s18581, s18490, s19978')
+        
+if __name__ == '__main__':
     main()
